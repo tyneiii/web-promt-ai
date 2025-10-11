37a3e3f701814057dd6875d6ef5092dcfc6b86f0
Pham Trung Kien <kien.pt.64cntt@ntu.edu.vn>
2025-10-11 21:31:42 +0700

    update create post view, home view

--- views/user/layout/header.php
+++ views/user/layout/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Prompt AI Share</title>
-  <link rel="stylesheet" href="/web-promt-ai/public/css/home.css">
+  <link rel="stylesheet" href="/ProjectPHP/web-promt-ai/public/css/home.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
