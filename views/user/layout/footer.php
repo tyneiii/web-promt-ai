6f819bd88dbb97813f56ba3d92abcc9606d014cf
NTQuangMinh <minh931ddt@gmail.com>
2025-11-12 23:20:56 +0700

    ads

--- views/user/layout/footer.php
+++ views/user/layout/footer.php
@@ -1,3 +1,19 @@
-
 </body>
+    <div id="sticky-ad-banner" style="text-align: center; padding: 15px 0;">
+        <span id="close-ad-btn" onclick="closeStickyAd()">&times;</span>
+        <a href="../../api/count_ad_click.php?ad=1" target="_blank"> 
+            <img src="../../public/img/ads.jpg" alt="Quảng cáo">
+        </a>
+    </div>
 </html>
+<script>
+    function closeStickyAd() {
+        // Lấy phần tử banner
+        var adBanner = document.getElementById('sticky-ad-banner');
+        
+        // Ẩn nó đi
+        if (adBanner) {
+            adBanner.style.display = 'none';
+        }
+    }
+</script>
