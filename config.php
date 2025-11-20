e1d1bed7f380521aa4f0ca91f3c854b7613ad7ca
Aliahoang2710 <hoanganhthu271004@gmail.com>
2025-11-20 23:14:37 +0700

    adding function login/register with google

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
-     $servername = "localhost";   
-        $username = "root";          
-        $password = "";              
-        $database = "prompt_database";       
-        $conn = new mysqli($servername, $username, $password, $database);
+    $clientID = "957982290942-ps5dku2el8nick62eh49h2e5pd7v1bl6.apps.googleusercontent.com";
+    $clientSecret = "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";
+    $redirectUri = "http://localhost:8080/baiTap/web-promt-ai/controller/authentification/GoogleController.php";
+    $servername = "localhost";   
+    $username = "root";          
+    $password = "";              
+    $database = "prompt_database";       
+    $conn = new mysqli($servername, $username, $password, $database);
     if($conn->error){
         die('Error');
     }
